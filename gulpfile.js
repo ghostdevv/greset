a36ce815f6d55d567841941f4f4fafc3ebec4dd0
GHOST <ghostdevbusiness@gmail.com>
2021-09-12 20:48:43 +0100

    feat;: gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import gulp from 'gulp';
+
+const sass = gulpSass(dartSass);
+
+export const build = () =>
+    gulp.src('./scss/greset.scss').pipe(sass()).pipe(gulp.dest('./dist'));
+
+export const dev = () => gulp.watch('./scss/**.scss', build);
