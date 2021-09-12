ba604629a6a7ab76e927ad2905afcdea2917afbe
GHOST <ghostdevbusiness@gmail.com>
2021-09-12 20:55:47 +0100

    feat: postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
 import gulpSass from 'gulp-sass';
+import cssnano from 'cssnano';
 import dartSass from 'sass';
 import gulp from 'gulp';
 
 const sass = gulpSass(dartSass);
 
-export const build = () =>
-    gulp.src('./scss/greset.scss').pipe(sass()).pipe(gulp.dest('./dist'));
+const plugins = [
+    cssnano(),
+    autoprefixer({ overrideBrowserslist: ['defaults and last 4 version'] }),
+];
 
-export const dev = () =>
-    gulp.watch(
-        './scss/**.scss',
-        gulp.src('./scss/greset.scss').pipe(sass()).pipe(gulp.dest('./dist')),
-    );
+gulp.task('build', () =>
+    gulp
+        .src('./scss/greset.scss')
+        .pipe(sass())
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./dist')),
+);
+
+gulp.task('dev', () => gulp.watch('./scss/**.scss', gulp.series('build')));
